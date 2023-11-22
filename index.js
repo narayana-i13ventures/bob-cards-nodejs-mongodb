19dea2e73a96bf9edc8a20db4eabf16e6f695715
LvsalnNarayana <lvsalnnarayana@gmail.com>
2023-11-22 23:11:51 +0000

    remove cpu

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-import cluster from 'cluster';
-import os from 'os';
 import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
@@ -16,61 +14,47 @@ import Card from './Models/CardModel.js';
 
 dotenv.config();
 
-const numCPUs = os.cpus().length;
+const app = express();
+const port = process.env.PORT;
 
-if (cluster.isMaster) {
-    // Create a master process that forks workers for each CPU core
-    for (let i = 0; i < numCPUs; i++) {
-        cluster.fork();
+// Connect to MongoDB (replace 'your-connection-string' with your actual MongoDB connection string)
+connectToDatabase('mongodb+srv://narayana:Narayana1997@bob.ajbel4v.mongodb.net/data');
+
+// Middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+app.use(helmet());
+
+// Basic route that returns 'Hello'
+app.get('/', (req, res) => {
+    res.send('Hello');
+});
+
+app.use('/company', companyRouter);
+app.use('/future_1', Future1BMCRouter);
+app.use('/future_2', Future2BMCRouter);
+app.use('/future_3', Future3BMCRouter);
+app.use('/future_1', Future1CVPRouter);
+app.use('/', ThinkBeyondRouter);
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something went wrong!');
+});
+app.post('/updateCard', async (req, res) => {
+    try {
+        const { data } = req?.body;
+        const updatedCard = await Card.updateCard(data);
+        res.status(200).json(updatedCard);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Error updating Card' });
     }
+});
 
-    cluster.on('exit', (worker, code, signal) => {
-        console.log(`Worker ${worker.process.pid} died`);
-    });
-} else {
-    // Each worker process will run the following code
-    const app = express();
-    const port = process.env.PORT;
-
-    // Connect to MongoDB (replace 'your-connection-string' with your actual MongoDB connection string)
-    connectToDatabase('mongodb+srv://narayana:Narayana1997@bob.ajbel4v.mongodb.net/data');
-
-    // Middleware
-    app.use(bodyParser.json());
-    app.use(bodyParser.urlencoded({ extended: true }));
-    app.use(cors());
-    app.use(helmet());
-
-    // Basic route that returns 'Hello'
-    app.get('/', (req, res) => {
-        res.send('Hello');
-    });
-
-    app.use('/company', companyRouter);
-    app.use('/future_1', Future1BMCRouter);
-    app.use('/future_2', Future2BMCRouter);
-    app.use('/future_3', Future3BMCRouter);
-    app.use('/future_1', Future1CVPRouter);
-    app.use('/', ThinkBeyondRouter);
-
-    // Error handling middleware
-    app.use((err, req, res, next) => {
-        console.error(err.stack);
-        res.status(500).send('Something went wrong!');
-    });
-    app.post('/updateCard', async (req, res) => {
-        try {
-            const { data } = req?.body;
-            const updatedCard = await Card.updateCard(data);
-            res.status(200).json(updatedCard);
-        } catch (error) {
-            console.error(error);
-            res.status(500).json({ error: 'Error updating Card' });
-        }
-    });
-
-    // Start the server for each worker
-    app.listen(port, () => {
-        // console.log(`Worker ${process.pid} is running on port ${port}`);
-    });
-}
+// Start the server for each worker
+app.listen(port, () => {
+    // console.log(`Worker ${process.pid} is running on port ${port}`);
+});
